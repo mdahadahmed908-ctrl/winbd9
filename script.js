eddb63281f92d66abda16d2a928671b2d0ece7c9
mdahadahmed908-ctrl <mdahadahmed908@gmail.com>
2026-07-30 07:56:48 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ document.querySelectorAll("button").forEach(button => {
     alert(button.innerText + " button clicked!");
   });
 });
+.buttons{
+    display:flex;
+    flex-direction:column;
+    gap:15px;
+    padding:20px;
+}
+
+.buttons button{
+    padding:15px;
+    font-size:18px;
+    border:none;
+    border-radius:12px;
+    background:#00b894;
+    color:white;
+    font-weight:bold;
+}
+
+.buttons button:active{
+    background:#019875;
+}
